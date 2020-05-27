f93ca896422c51ce236f612ceefd173547595583
nalbam <me@nalbam.com>
2020-05-27 14:05:51 +0900

    terraform-aws versions

--- examples/dev-null/main.tf
+++ examples/dev-null/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  # source = "github.com/mzcdev/terraform-aws-vpc?ref=v0.12.1"
+  # source = "github.com/mzcdev/terraform-aws-vpc?ref=v0.12.39"
   source = "../../"
 
   region = var.region
